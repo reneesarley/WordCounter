2e48e755dad67ef6f13e453a4f6a012554281805
Renee Sarley <reneesarley@gmail.com>
2018-06-29 15:38:19 -0700

    add while loop to Main method to loop to back to start

--- WordCounter/Models/Program.cs
+++ WordCounter/Models/Program.cs
@@ -7,17 +7,28 @@ namespace WordCounter
     {
         static void Main()
         {
-            RepeatCounter newRepeatCounter = new RepeatCounter();
-            Console.WriteLine("Enter a word you would like to search for");
-            string usersWord = Console.ReadLine();
-            Console.WriteLine("Enter a string you'd like searched for your word and I'll tell you how many times the word appears.");
-            string usersString = Console.ReadLine();
-            newRepeatCounter.SetWordToFind(usersWord);
-            newRepeatCounter.SetStringToSearch(usersString);
-            newRepeatCounter.UpdateWordCounter();
-            int wordCount = newRepeatCounter.GetWordCounter();
-            Console.WriteLine("'{0}' appears {1} times in '{2}'", usersWord, wordCount, usersString);
-
+            while (true)
+            {
+                RepeatCounter newRepeatCounter = new RepeatCounter();
+                Console.WriteLine("Enter a word you would like to search for");
+                //newRepeatCounter.GetUsersWord();
+                string usersWord = Console.ReadLine();
+                newRepeatCounter.SetWordToFind(usersWord);
+                if (newRepeatCounter.VerifyJustOneWord())
+                {
+                    Console.WriteLine("Enter a string you'd like searched for your word and I'll tell you how many times the word appears.");
+                    string usersString = Console.ReadLine();
+                    newRepeatCounter.SetWordToFind(usersWord);
+                    newRepeatCounter.SetStringToSearch(usersString);
+                    newRepeatCounter.UpdateWordCounter();
+                    int wordCount = newRepeatCounter.GetWordCounter();
+                    Console.WriteLine("'{0}' appears {1} times in '{2}'", usersWord, wordCount, usersString);
+                }
+                else
+                {
+                    Console.WriteLine("Please only enter one word");
+                }
+            }
         }
     }
 }
